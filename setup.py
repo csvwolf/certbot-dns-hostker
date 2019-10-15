fe9ec9e2e77d15e5b7e19d0a4e5299ef1b5bebee
SkyAo <csvwolf@qq.com>
2019-10-15 22:48:31 +0800

    feat: add readme as long description in pip

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
+import io
+
 from setuptools import setup
 from setuptools import find_packages
 
 setup(
-  name='certbot-dns-hostker',
-  version='0.0.2',
-  description='Certbot for Hostker DNS',
-  author='SkyAo',
-  author_email='csvwolf@qq.com',
-  url='https://www.codesky.me',
-  packages=find_packages(),
-  entry_points={
-    'certbot.plugins': [
-      'dns-hostker = certbot_dns_hostker.dns_hostker:Authenticator'
-    ]
-  }
+    name='certbot-dns-hostker',
+    version='0.0.2',
+    description='Certbot for Hostker DNS',
+    author='SkyAo',
+    author_email='csvwolf@qq.com',
+    url='https://www.codesky.me',
+    packages=find_packages(),
+    long_description=io.open('README.md', encoding='utf-8').read(),
+    entry_points={
+        'certbot.plugins': [
+            'dns-hostker = certbot_dns_hostker.dns_hostker:Authenticator'
+        ]
+    }
 )
